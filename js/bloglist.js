8d3f62f7c70cfc15234ed720f2c985e5eb17806a
Nitin Bajpai <nitinbajpai110@gmail.com>
2019-07-05 23:35:49 +0530

    BLOGLIST FILE HAS BEEN ADDED WITH HEADER SIGN IN SIGN UP FUNCTIIONALITY AND TWO DIV ELEMENTS IN PAIR

---
+++ js/bloglist.js
@@ -0,0 +1,16 @@
+document.getElementById('lnk').addEventListener("click",function(){
+    $('#exampleModal').modal('hide')
+});
+document.getElementById('allpost').addEventListener("click",function(){
+    location.href="html/bloglist.html";
+})
+function verify(){
+var a1=document.getElementById('password1').value;
+var a2=document.getElementById('password2').value;
+if(a1!=a2)
+{
+    alert("PASSWORD AND CONFIRM PASSWORRD DIDN'T MATCHED");
+}
+else
+alert('YOUR ACCOUNT HAS BEEN CREATED SUCCESSFULLY');
+}
