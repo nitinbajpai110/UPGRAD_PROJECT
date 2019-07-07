18df2ddffe7c3e8879acf73e0ef026ab5fdeae0a
Nitin Bajpai <nitinbajpai110@gmail.com>
2019-07-07 14:54:59 +0530

    POST PAGE HAS BEEN ADDED WITH EDIT/SAVE FUNCTIONALITY

--- js/post.js
+++ js/post.js
@@ -42,8 +42,28 @@ document.getElementById("commentCount").innerHTML=h+" people have liked this!";
 }
 
 // EDIT BUTTON FUNCTIONALITY
+var t=0;
 function edit(){
+    if(t==0)
+    {
     document.getElementById("blogBody").setAttribute("contentEditable","true");
     document.getElementById("blogTitleNew").setAttribute("contentEditable","true");
+    document.getElementById("editicon").innerHTML=" Save";
+    document.getElementById("editicon").setAttribute("class","fa fa-save");
+    t++;
+    document.getElementById("blogBody").setAttribute("style","border:1px solid blue");
+    document.getElementById("blogTitleNew").setAttribute("style","border:1px solid red");
+    }
+    else{
+        document.getElementById("blogBody").removeAttribute("contentEditable");
+        document.getElementById("blogTitleNew").removeAttribute("contentEditable");
+        document.getElementById("editicon").innerHTML="Edit";
+        document.getElementById("editicon").setAttribute("class","fa fa-edit");
+        t=0;
+        document.getElementById("blogBody").setAttribute("style","border:none");
+        document.getElementById("blogTitleNew").setAttribute("style","border:none");
+    }
+    
+
 
 }
