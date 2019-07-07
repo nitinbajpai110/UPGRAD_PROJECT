a3b57313f1c09193fb8acff40ee409a0411fd3c4
Nitin Bajpai <nitinbajpai110@gmail.com>
2019-07-07 15:45:23 +0530

    BOOTSTRAP FILES HAS BEEN ADDED

--- js/index.js
+++ js/index.js
@@ -1,15 +1,4 @@
-document.getElementById('lnk').addEventListener("click",function(){
-    $('#exampleModal').modal('hide')
-});
+
 document.getElementById('allpost').addEventListener("click",function(){
     location.href="html/bloglist.html";
 })
-function verify(){
-var a1=document.getElementById('password1').value;
-var a2=document.getElementById('password2').value;
-if(a1!=a2)
-{
-    alert("PASSWORD AND CONFIRM PASSWORRD DIDN'T MATCHED");
-}
-
-}
